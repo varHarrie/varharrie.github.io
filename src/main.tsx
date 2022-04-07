2769cf0152204743a5e8d6c5cfb446a33c149a17
varHarrie <varharrie@gmail.com>
2022-04-07 21:44:12 +0800

    feat: add env variables

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import BaseStyles from './styles/BaseStyles';
 
+document.title = import.meta.env.VITE_APP_TITLE;
+document.documentElement.classList.add('light');
+
 const rootEl = document.getElementById('root')!;
 const root = createRoot(rootEl);
 
---
+++ types/env.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  VITE_APP_TITLE: string;
+  VITE_GITHUB_URL: string;
+  VITE_GITHUB_ACCESS_TOKEN: string;
+  VITE_GITHUB_OWNER: string;
+  VITE_GITHUB_REPO: string;
+  VITE_GITHUB_MILESTONE_POSTS: string;
+  VITE_GITHUB_MILESTONE_SNIPPETS: string;
+}
