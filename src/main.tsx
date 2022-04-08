4de5ff89d017daa8dd84623ed848e034c6bb49cd
varHarrie <varharrie@gmail.com>
2022-04-08 20:11:54 +0800

    feat: add home view

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import BaseStyles from './styles/BaseStyles';
+import Home from './views/Home';
 
 document.title = import.meta.env.VITE_APP_TITLE;
 
@@ -17,7 +18,9 @@ root.render(
     <BaseStyles />
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<App />}>
+          <Route path="" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/views/Home.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import tw from 'twin.macro';
+
+import IconArrowRight from '~icons/ri/arrow-right-line';
+
+const Wrapper = tw.main`px-8 py-20 text-center`;
+
+const Title = tw.h2`text-4xl text-gray-600 font-bold leading-10`;
+
+const Description = tw.p`mt-4 text-gray-300`;
+
+const LinkButton = tw.button`
+  mt-10 py-3 px-6
+  bg-blue-500 active:bg-blue-600 text-blue-50 text-center
+  rounded-full outline-none transition-all
+`;
+
+export default function Home() {
+  const { t } = useTranslation();
+
+  return (
+    <Wrapper>
+      <Title>{t('intro.title')}</Title>
+      <Description>{t('intro.description')}</Description>
+      <Link to="/posts">
+        <LinkButton>
+          <span tw="mr-2">{t('intro.link')}</span>
+          <IconArrowRight />
+        </LinkButton>
+      </Link>
+    </Wrapper>
+  );
+}
